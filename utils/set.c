81e099673b501bf09edb932aff4e46a247b0b46a
sahil-1035 <sahil.us.1035@gmail.com>
2024-04-23 15:19:50 +0530

    Set now added for breakpoints

---
+++ utils/set.c
@@ -0,0 +1,91 @@
+#include "set.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+Set* create_set(unsigned int len)
+{
+	Set* new_set = (Set*) malloc( sizeof(Set) );
+	new_set->arr = (int*) malloc( len * sizeof(int) );
+	new_set->pointer = 0;
+	new_set->max_len = len;
+	return new_set;
+}
+
+void insert_set(Set* set, int val)
+{
+	if ( find_in_set(set, val) )
+		return;
+	set->arr[set->pointer] = val;
+	set->pointer++;
+}
+
+char* get_str_set(Set* set, char* buff)
+{
+	if ( set_is_empty(set) )
+		return "{}";
+
+	sprintf(buff, "{");
+	sprintf(buff, "%s %d", buff, set->arr[0]);
+	for(int i = 1; i < length_of_set(set) - 1; i++)
+	{
+		sprintf(buff, "%s, %d", buff, set->arr[i]);
+	}
+	sprintf(buff, "%s }", buff);
+	return buff;
+}
+
+bool find_in_set(Set* set, int val)
+{
+	for(int i = 0; i < length_of_set(set); i++)
+	{
+		if ( set->arr[i] == val )
+			return true;
+	}
+	return false;
+}
+
+void erase_set(Set* set, int val)
+{
+	int val_at = -1;
+	for(int i = 0; i < length_of_set(set); i++)
+	{
+		if ( set->arr[i] == val )
+		{
+			val_at = i;
+			break;
+		}
+	}
+	assert( val_at != -1 );
+
+	memcpy(set->arr + val_at, set->arr + val_at + 1, length_of_set(set) - val_at);
+	set->pointer--;
+}
+
+int get_set_max_len(Set* set)
+{
+	return set->max_len;
+}
+
+void delete_set(Set* set)
+{
+	free(set->arr);
+	free(set);
+}
+
+void clear_set(Set* set)
+{
+	set->pointer = 0;
+}
+
+unsigned int length_of_set(Set* set)
+{
+	return set->pointer + 1;
+}
+
+bool set_is_empty(Set* set)
+{
+	return !( (bool)set->pointer );
+}
---
+++ utils/set.h
@@ -0,0 +1,25 @@
+#ifndef SET_H
+#define SET_H
+
+#include <stdbool.h>
+
+typedef struct Set {
+	int* arr;
+	int pointer;
+	int max_len;
+} Set;
+
+Set* create_set(unsigned int len);
+void delete_set(Set* set);
+
+void insert_set(Set* set, int val);
+void erase_set(Set* set, int val);
+
+bool find_in_set(Set* set, int val);
+
+int get_set_max_len(Set* set);
+char* get_str_set(Set* set, char* buff);
+bool set_is_empty(Set* set);
+void clear_set(Set* set);
+unsigned int length_of_set(Set* set);
+#endif
